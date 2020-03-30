850f5aef4c3b92e51c5d60e2a2e0c7ef1f6b38f6
wenpin <wpchou@outlook.com>
2020-03-30 16:16:43 +0800

    use lazycat's nox lsp client

---
+++ init-lsp-mode.el
@@ -0,0 +1,20 @@
+(straight-use-package 'lsp-mode)
+(straight-use-package 'lsp-ui)
+(straight-use-package 'company-lsp)
+(setq company-minimum-prefix-length 1
+      company-idle-delay 0.0) ;; default is 0.2
+;; TODO double by 2 every time to test
+;; (setq gc-cons-threshold ) 
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(setq lsp-prefer-capf t)
+;; (setq lsp-idle-delay 0.500)
+(setq lsp-print-performance t)
+(setq lsp-log-io t)
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
+(with-eval-after-load 'lsp-mode
+  ;; :project/:workspace/:file
+  (setq lsp-diagnostics-modeline-scope :project)
+  (add-hook 'lsp-managed-mode-hook 'lsp-diagnostics-modeline-mode))
+
+(provide 'init-lsp.el)
--- init.el
+++ init.el
@@ -35,24 +35,10 @@
 
 ;;; programming
 ;; lsp rules all
-(straight-use-package 'lsp-mode)
-(straight-use-package 'lsp-ui)
-(straight-use-package 'company-lsp)
-(setq company-minimum-prefix-length 1
-      company-idle-delay 0.0) ;; default is 0.2
-;; TODO double by 2 every time to test
-;; (setq gc-cons-threshold ) 
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
-(setq lsp-prefer-capf t)
-;; (setq lsp-idle-delay 0.500)
-(setq lsp-print-performance t)
-(setq lsp-log-io t)
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
-(with-eval-after-load 'lsp-mode
-  ;; :project/:workspace/:file
-  (setq lsp-diagnostics-modeline-scope :project)
-  (add-hook 'lsp-managed-mode-hook 'lsp-diagnostics-modeline-mode))
+;; (require 'init-lsp-mode)
+(straight-use-package '(nox :host github :repo "manateelazycat/nox"))
+(dolist (hook (list 'js-mode-hook))
+  (add-hook hook '(lamda () (nox-ensure))))
 
 (straight-use-package 'typescript-mode)
 
@@ -69,7 +55,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(leuven))
- '(recentf-mode t))
+ '(recentf-mode t)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
