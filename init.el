68c6355e20002cff4fa3bdad5a8633a02658cd51
wenpin <wpchou@outlook.com>
2020-04-17 18:34:51 +0800

    REBOOTING my EMACS life!

--- init.el
+++ init.el
@@ -1,10 +1,11 @@
-;;; init.el --- wenpin's emacs.d                     -*- lexical-binding: t; -*-
+;;; pre-init
+;; M-SPC is key to my emacs world
+(global-unset-key (kbd "M-SPC"))
+;; accelerate loading init files, will be reset by gcmh
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
 
-;; Copyright (C) 2020 wenpin
-
-;; Author:  wenpin <w@shamingming.com>
-
-;; bootstrap straight.el
+;;; bootstrap straight.el and use-package
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -17,17 +18,105 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
-;; use-package
 (setq straight-default-vc 'git)
+;; use-package powers all
 (straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+;; decouple code related to straight.el and use-package
+(setq straight-use-package-by-default nil)
+;; install and load packages by straight.el
+;; straight.el loads auto-load functions only
+(load (concat (file-name-directory (or load-file-name buffer-file-name)) "package"))
+;; personal lisp
+(add-to-list 'load-path (concat (file-name-directory (or load-file-name buffer-file-name)) "lisp"))
+(add-to-list 'load-path (concat (file-name-directory (or load-file-name buffer-file-name)) "site-lisp"))
 
-;; oh, it's magit
-(straight-use-package 'magit)
+;;; lazycat is lazy
+(use-package auto-save
+  :config
+  (setq auto-save-silent t
+	auto-save-delete-trailing-whitespace t)
+  (auto-save-enable))
+;;; waiting to explore
+(use-package company
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'company-mode)
+  (add-hook 'js-mode-hook #'company-mode)
+  (add-hook 'typescript-mode-hook #'company-mode))
+;;; live in emacs
+(use-package eaf)
+;;; simple and intuitive
+(use-package expand-region :bind ("M-SPC v" . er/expand-region))
+;;; currently for snails only
+(use-package fuz :config (unless (require 'fuz-core nil t) (fuz-build-and-load-dymod)))
+;;; oh, it's magit
+(use-package magit :bind ("M-SPC g s" . magit-status))
+;;; it's spc, spc
+(use-package snails
+  :after fuz
+  :bind ("M-SPC SPC" . snails)
+;  :config (snails '(snails-backend-buffer snails-backend-recentf))
+  )
+;;; lsp rules all
+(use-package nox
+  :after (company posframe)
+  :config
+  (dolist (hook (list
+		 'js-mode-hook
+		 'typescript-mode-hook
+		 ))
+    (add-hook hook '(lamda () (nox-ensure))))
+)
+;;; my org, my life
+(use-package org
+  :custom
+  (org-directory "~/org")
+  (org-agenda-span 'month)
+  (org-agenda-show-future-repeats 'next)
+  (org-agenda-files '("~/org/orgzly"))
+  (org-agenda-log-mode-items '(closed))
+  (org-todo-keywords
+      (quote ((sequence "TODO(T)" "NEXT(n)" "|" "DONE(t)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c!)" "PHONE" "MEETING")
+              ;; 下面这行会导致 spacemacs 的 org headings 效果消失，因为关键词重复
+              ;; (type "EXPERIENCE(e) DEBUG(d) | "DONE")
+              (type "EXPERIENCE(e)" "DEBUG(d)" "BOOKMARK(b)" "MARKBOOK(m)")
+              )))
+  (org-default-notes-file (concat org-directory "/orgzly/Inbox.org"))
+  (org-capture-templates
+      '(
+        ("t" "TODO" entry (file org-default-notes-file)
+         "* TODO %?\n  :PROPERTIES:\n  :CREATED:  %U\n  :END:\n%i\n  %a")
+        ("n" "NOTE" entry (file+headline org-default-notes-file "NOTES")
+         "* %?\n  :PROPERTIES:\n  :CREATED:  %U\n  :CONTEXT:  %a\n:END:\n%i\n")
+        ("j" "js source code" entry (file+headline org-default-notes-file "NOTES")
+         "* %?\n  :PROPERTIES:\n  :CREATED:  %U\n  :CONTEXT:  %a\n:END:\n  #+begin_src js\n%i  #+end_src\n")
+        ("s" "source code" entry (file+headline org-default-notes-file "NOTES")
+         "* %?\n  :PROPERTIES:\n  :CREATED:  %U\n  :CONTEXT:  %a\n:END:\n  #+begin_src %^{source language}\n%i%?  #+end_src\n")
+        ("g" "template group")
+        ("ga" "Template Group A holder" entry (file+headline org-default-notes-file "NOTES")
+         "* %?\n  :PROPERTIES:\n  :CREATED:  %U\n  :CONTEXT:  %a\n:END:\n  #+begin_src %^{source language}\n%i%?  #+end_src\n")
+        ))
+  )
+;;; difference between heaven and hell
+(use-package paredit
+  :commands (enable-paredit-mode)
+  :init
+    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+    :config
+    (require 'eldoc)
+    (eldoc-add-command
+     'paredit-backward-delete
+     'paredit-close-round)
+    )
+;;; modern emacs
+(use-package posframe)
 
-;; rime, THE input method
-(straight-use-package '(rime :host github :repo "DogLooksGood/emacs-rime" :files ("*.el" "Makefile" "lib.c")))
+;;; rime, THE input method
 ;; mostly copy from https://github.com/cnsunyour/.doom.d/blob/develop/modules/cnsunyour/chinese/config.el
 (use-package rime
   :bind
@@ -46,27 +135,17 @@
   (unless (fboundp 'rime--posframe-display-content)
     (error "Function `rime--posframe-display-content' is not available.")))
 
-;; my org, my life
-(straight-use-package 'org)
+(use-package typescript-mode
+  :custom (typescript-indent-level 2))
+(use-package which-key :config (which-key-mode 1))
 
-;; utilities
-(straight-use-package 'which-key)
-(which-key-mode)
+;;; reset gc
+(use-package gcmh :config (gcmh-mode 1))
 
-
-;;; programming
-;; lsp rules all
-;; (require 'init-lsp-mode)
-(straight-use-package '(nox :host github :repo "manateelazycat/nox"))
-
-(dolist (hook (list 'js-mode-hook))
-  (add-hook hook '(lamda () (nox-ensure))))
-
-(straight-use-package 'typescript-mode)
-
-
-;;; live in emacs
-(straight-use-package '(emacs-application-framework :host github :repo "manateelazycat/emacs-application-framework" :files ("app" "core" "*.el" "*.py")))
+(global-set-key (kbd "M-SPC f f") 'find-file)
+(global-set-key (kbd "M-SPC q q") 'save-buffers-kill-terminal)
+(tool-bar-mode -1)
+(toggle-frame-maximized)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'no-error 'no-message)
---
+++ site-lisp/auto-save.el
@@ -0,0 +1,112 @@
+;;; copied from https://github.com/manateelazycat/auto-save/blob/25ebe49d3da5ff050d97bbda408083375d78e479/auto-save.el
+
+(defgroup auto-save nil
+  "Save file(s) automatically when emacs is idle."
+  :group 'auto-save)
+
+(defcustom auto-save-idle 1
+  "How many seconds after emacs is idle to save file."
+  :type 'integer
+  :group 'auto-save)
+
+(defcustom auto-save-silent nil
+  "Do not notify in minibuffer if not-nil."
+  :type 'boolean
+  :group 'auto-save)
+
+(defcustom auto-save-delete-trailing-whitespace nil
+  "Delete trailing whitespace except current line before saving if not-nil."
+  :type 'boolean
+  :group 'auto-save)
+
+(defvar auto-save-disable-predicates nil
+  "Disable auto-save in these cases")
+
+(setq auto-save-default nil)
+
+(defun auto-save-buffers ()
+  (interactive)
+  (let ((autosave-buffer-list))
+    (ignore-errors
+      (save-excursion
+        (dolist (buf (buffer-list))
+          (set-buffer buf)
+          (when (and
+                 ;; Buffer associate with a filename?
+                 (buffer-file-name)
+                 ;; Buffer is modifiable?
+                 (buffer-modified-p)
+                 ;; Yassnippet is not active?
+                 (or (not (boundp 'yas--active-snippets))
+                     (not yas--active-snippets))
+                 ;; Company is not active?
+                 (or (not (boundp 'company-candidates))
+                     (not company-candidates))
+                 ;; tell auto-save don't save
+                 (not (seq-some (lambda (predicate)
+                                  (funcall predicate))
+                                auto-save-disable-predicates)))
+            (push (buffer-name) autosave-buffer-list)
+            (if auto-save-silent
+                ;; `inhibit-message' can shut up Emacs, but we want
+                ;; it doesn't clean up echo area during saving
+                (with-temp-message ""
+                  (let ((inhibit-message t))
+                    (basic-save-buffer)))
+              (basic-save-buffer))
+            ))
+        ;; Tell user when auto save files.
+        (unless auto-save-silent
+          (cond
+           ;; It's stupid tell user if nothing to save.
+           ((= (length autosave-buffer-list) 1)
+            (message "# Saved %s" (car autosave-buffer-list)))
+           ((> (length autosave-buffer-list) 1)
+            (message "# Saved %d files: %s"
+                     (length autosave-buffer-list)
+                     (mapconcat 'identity autosave-buffer-list ", ")))))
+        ))))
+
+(defun auto-save-delete-trailing-whitespace-except-current-line ()
+  (interactive)
+  (when auto-save-delete-trailing-whitespace
+    (let ((begin (line-beginning-position))
+          (end (line-end-position)))
+      (save-excursion
+        (when (< (point-min) begin)
+          (save-restriction
+            (narrow-to-region (point-min) (1- begin))
+            (delete-trailing-whitespace)))
+        (when (> (point-max) end)
+          (save-restriction
+            (narrow-to-region (1+ end) (point-max))
+            (delete-trailing-whitespace)))))))
+
+(defvar auto-save-timer nil)
+
+(defun auto-save-set-timer ()
+  "Set auto-saveTime. Cancel all previous timers."
+  (auto-save-cancel-timer)
+  (setq auto-save-timer
+	(run-with-idle-timer auto-save-idle t 'auto-save-buffers)))
+
+(defun auto-save-cancel-timer ()
+  (when auto-save-timer
+    (cancel-timer auto-save-timer)
+    (setq auto-save-timer nil)))
+
+
+(defun auto-save-enable ()
+  (interactive)
+  (auto-save-set-timer)
+  (add-hook 'before-save-hook 'auto-save-delete-trailing-whitespace-except-current-line)
+  (add-hook 'before-save-hook 'font-lock-flush)
+  )
+
+(defun auto-save-disable ()
+  (auto-save-cancel-timer)
+  (remove-hook 'before-save-hook 'auto-save-delete-trailing-whitespace-except-current-line)
+  (remove-hook 'before-save-hook 'font-lock-flush)
+  )
+
+(provide 'auto-save)
