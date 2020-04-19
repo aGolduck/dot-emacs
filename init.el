9cce2e323201c6f1d7b9e54fbc7a93e3e665a17c
wenpin <wpchou@outlook.com>
2020-04-19 22:02:17 +0800

    set PATH for mac manually

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
 ;;; pre-init
+;; set PATH manually, https://blog.galeo.me/path-environment-variable-on-mac-os-x-emacs-app.html
+;; TODO: add othen variables like MANPATH
+;; when more and more env vars need to be set, referece doom-load-envvars-file
+(when (and (eq system-type 'darwin) window-system)
+  (setenv "PATH"
+	  (concat "~/.rbenv/shims:~/.rbenv/bin:~/.gem/ruby/2.7.0/bin:~/.cargo/bin:~/bin:~/.local/bin:/usr/local/opt/node@10/bin:/usr/local/opt/ruby/bin:/usr/pkg/bin:/usr/pkg/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/snap/bin:/Applications/VMware Fusion.app/Contents/Public:/Library/TeX/texbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:~/Library/Android/sdk/tools:~/Library/Android/sdk/platform-tools:~/.antigen/bundles/robbyrussell/oh-my-zsh/lib:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/colored-man-pages:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/extract:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/z:~/.antigen/bundles/zsh-users/zsh-syntax-highlighting:~/.antigen/bundles/zsh-users/zsh-completions:~/.antigen/bundles/zsh-users/zsh-autosuggestions:~/.antigen/bundles/denisidoro/navi"
+		  path-separator (getenv "PATH")))
+  (setq exec-path (split-string (getenv "PATH") path-separator)))
 ;; M-SPC is key to my emacs world
 (global-unset-key (kbd "M-SPC"))
 ;; accelerate loading init files, will be reset by gcmh
