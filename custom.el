3086054c99b701bc5c1326698f27011a79bfd3b1
wenpin <wpchou@outlook.com>
2020-04-22 18:35:14 +0800

    set js-indent and font size

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  '(auto-save-default nil)
  '(auto-save-list-file-prefix nil)
  '(default-input-method "rime" nil nil "Customized with use-package rime")
+ '(js-indent-level 2)
  '(line-spacing 0.2)
  '(mac-command-modifier 'super)
  '(mac-option-modifier 'meta)
@@ -21,5 +22,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "JetBrains Mono" :foundry "JB  " :slant normal :weight normal :height 102 :width normal))))
+ '(default ((t (:family "JetBrains Mono" :foundry "JB  " :slant normal :weight normal :height 112 :width normal))))
  '(fixed-pitch-serif ((t (:family "Courier Prime")))))
