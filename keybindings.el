dffe285711cdaa1f037ab2a1a7bc6d39fa79d5a7
wenpin <wpchou@outlook.com>
2020-04-23 16:57:44 +0800

    add keybindings

--- keybindings.el
+++ keybindings.el
@@ -4,8 +4,21 @@
 (global-set-key (kbd "M-SPC w d") 'delete-window)
 (global-set-key (kbd "M-SPC w D") 'delete-other-windows)
 (global-set-key (kbd "M-SPC w s") 'split-window-right)
+(global-set-key (kbd "M-SPC w S") (lambda () (interactive) (split-window-right) (other-window 1)))
 (global-set-key (kbd "M-SPC w v") 'split-window-below)
+(global-set-key (kbd "M-SPC w V") (lambda () (interactive) (split-window-below) (other-window 1)))
 (global-set-key (kbd "M-SPC w o") 'other-window)
 (global-set-key (kbd "M-SPC q q") 'save-buffers-kill-terminal)
+
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line.
+Position the cursor at it's beginning, according to the current mode."
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+(global-set-key (kbd "M-o") 'smart-open-line-above)
+
 
 (provide 'init-keybindings)
