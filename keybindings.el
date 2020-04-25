3119226d0b363dd472a3a50bd5d33f17d9069939
wenpin <wpchou@outlook.com>
2020-04-25 08:18:13 +0800

    add keybindings

--- keybindings.el
+++ keybindings.el
@@ -1,6 +1,7 @@
 (global-set-key (kbd "M-SPC b s") 'save-some-buffers)
 (global-set-key (kbd "M-SPC b k") 'kill-buffer)
 (global-set-key (kbd "M-SPC f f") 'find-file)
+(global-set-key (kbd "M-SPC f F") 'find-file-other-window)
 (global-set-key (kbd "M-SPC w d") 'delete-window)
 (global-set-key (kbd "M-SPC w D") 'delete-other-windows)
 (global-set-key (kbd "M-SPC w s") 'split-window-right)
@@ -8,6 +9,7 @@
 (global-set-key (kbd "M-SPC w v") 'split-window-below)
 (global-set-key (kbd "M-SPC w V") (lambda () (interactive) (split-window-below) (other-window 1)))
 (global-set-key (kbd "M-SPC w o") 'other-window)
+(global-set-key (kbd "M-SPC w O") (lambda () (interactive) (other-window -1)))
 (global-set-key (kbd "M-SPC q q") 'save-buffers-kill-terminal)
 
 (defun smart-open-line-above ()
