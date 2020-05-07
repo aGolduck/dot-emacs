1737f6c61d79378c3d1fc289191c73c15a2a6d4d
wenpin <wpchou@outlook.com>
2020-05-07 16:02:53 +0800

    refactor packages.el

--- packages.el
+++ packages.el
@@ -1,42 +1,48 @@
 ;;; 包安装与配置分离，利于包版本集中管理，利于灵活调整包管理工具
-(straight-use-package 'avy)
-(straight-use-package '(color-rg :host github :repo "manateelazycat/color-rg"))
-(straight-use-package 'company)
-(straight-use-package 'company-tabnine)
-;; ivy, counsel and swiper belongs to the same repo
-;; but straight.el builds them into different packages
-(straight-use-package 'counsel)
-(straight-use-package 'diminish)
-(straight-use-package 'dotenv-mode)
-(straight-use-package 'eglot)
-(straight-use-package '(emacs-application-framework :host github :repo "manateelazycat/emacs-application-framework" :files ("app" "core" "*.el" "*.py")))
-(straight-use-package 'expand-region)
-(straight-use-package 'eyebrowse)
-(straight-use-package '(flymake-posframe :host github :repo "Ladicle/flymake-posframe"))
-(straight-use-package '(fuz :host github :repo "rustify-emacs/fuz.el" :files ("src" "Cargo*" "*.el")))
-(straight-use-package 'gcmh)
-(straight-use-package 'git-link)
-(straight-use-package 'graphql-mode)
-(straight-use-package 'hl-todo)
-(straight-use-package 'ivy)
-(straight-use-package 'ivy-posframe)
-(straight-use-package 'lsp-mode)
-(straight-use-package 'magit)
-(straight-use-package 'magit-todos)
-(straight-use-package 'markdown-mode)
-(straight-use-package '(nox :host github :repo "manateelazycat/nox"))
-(straight-use-package 'olivetti)
-(straight-use-package 'org)
-(straight-use-package 'org-journal)
-(straight-use-package '(paredit :repo "http://mumble.net/~campbell/git/paredit.git/"))
-(straight-use-package 'posframe)
-(straight-use-package 'projectile)
-(straight-use-package '(rime :host github :repo "DogLooksGood/emacs-rime" :files ("*.el" "Makefile" "lib.c")))
-(straight-use-package '(snails :host github :repo "manateelazycat/snails" :fork (:host nil :repo "git@github.com:wpchou/snails.git") :files ("*.el" "*.sh" "*.ps1") :no-byte-compile t))
-(straight-use-package 'swiper)
-(straight-use-package 'vterm)
-(straight-use-package 'typescript-mode)
-(straight-use-package 'yaml-mode)
-(straight-use-package 'yasnippet)
+
+(dolist (wenpin-package
+         (list
+          'avy
+          '(color-rg :host github :repo "manateelazycat/color-rg")
+          'company
+          'company-tabnine
+          ;; ivy, counsel and swiper belongs to the same repo
+          ;; but straight.el builds them into different packages
+          'counsel
+          'diminish
+          'dotenv-mode
+          'eglot
+          '(emacs-application-framework :host github :repo "manateelazycat/emacs-application-framework" :files ("app" "core" "*.el" "*.py"))
+          'expand-region
+          'eyebrowse
+          '(flymake-posframe :host github :repo "Ladicle/flymake-posframe")
+          '(fuz :host github :repo "rustify-emacs/fuz.el" :files ("src" "Cargo*" "*.el"))
+          'gcmh
+          'git-link
+          'graphql-mode
+          'hl-todo
+          'ivy
+          'ivy-posframe
+          'lsp-mode
+          'magit
+          'magit-todos
+          'markdown-mode
+          '(nox :host github :repo "manateelazycat/nox")
+          'olivetti
+          'org
+          'org-journal
+          '(paredit :repo "http://mumble.net/~campbell/git/paredit.git/")
+          'posframe
+          'projectile
+          '(rime :host github :repo "DogLooksGood/emacs-rime" :files ("*.el" "Makefile" "lib.c"))
+          '(snails :host github :repo "manateelazycat/snails" :fork (:host nil :repo "git@github.com:wpchou/snails.git") :files ("*.el" "*.sh" "*.ps1") :no-byte-compile t)
+          'swiper
+          'vterm
+          'typescript-mode
+          'yaml-mode
+          'yasnippet
+          ))
+  (straight-use-package wenpin-package))
+
 
 (provide 'init-packages)
