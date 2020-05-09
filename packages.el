4c884123ccf2784e0fbe8558b7af1235ef28bc84
wenpin <wpchou@outlook.com>
2020-05-09 12:48:16 +0800

    order packages by sort-line

--- packages.el
+++ packages.el
@@ -2,22 +2,24 @@
 
 (dolist (wenpin-package
          (list
+          '(color-rg :host github :repo "manateelazycat/color-rg")
+          '(emacs-application-framework :host github :repo "manateelazycat/emacs-application-framework" :files ("app" "core" "*.el" "*.py"))
+          '(flymake-posframe :host github :repo "Ladicle/flymake-posframe")
+          '(fuz :host github :repo "rustify-emacs/fuz.el" :files ("src" "Cargo*" "*.el"))
+          '(nox :host github :repo "manateelazycat/nox")
+          '(paredit :repo "http://mumble.net/~campbell/git/paredit.git/")
+          '(rime :host github :repo "DogLooksGood/emacs-rime" :files ("*.el" "Makefile" "lib.c"))
+          '(snails :host github :repo "manateelazycat/snails" :fork (:host nil :repo "git@github.com:wpchou/snails.git") :files ("*.el" "*.sh" "*.ps1") :no-byte-compile t)
           'avy
           'ccls
-          '(color-rg :host github :repo "manateelazycat/color-rg")
           'company
-          ;; 'company-tabnine
-          ;; ivy, counsel and swiper belongs to the same repo
-          ;; but straight.el builds them into different packages
-          'counsel
+          'counsel ;; ivy, counsel and swiper belongs to the same repo, but straight.el builds them into different packages
           'diminish
           'dotenv-mode
           'eglot
-          '(emacs-application-framework :host github :repo "manateelazycat/emacs-application-framework" :files ("app" "core" "*.el" "*.py"))
+          'esh-autosuggest
           'expand-region
           'eyebrowse
-          '(flymake-posframe :host github :repo "Ladicle/flymake-posframe")
-          '(fuz :host github :repo "rustify-emacs/fuz.el" :files ("src" "Cargo*" "*.el"))
           'gcmh
           'git-link
           'graphql-mode
@@ -30,23 +32,20 @@
           'magit
           'magit-todos
           'markdown-mode
-          '(nox :host github :repo "manateelazycat/nox")
           'olivetti
           'org
           'org-journal
-          '(paredit :repo "http://mumble.net/~campbell/git/paredit.git/")
           'posframe
           'projectile
-          '(rime :host github :repo "DogLooksGood/emacs-rime" :files ("*.el" "Makefile" "lib.c"))
           'rust-mode
-          '(snails :host github :repo "manateelazycat/snails" :fork (:host nil :repo "git@github.com:wpchou/snails.git") :files ("*.el" "*.sh" "*.ps1") :no-byte-compile t)
           'sudo-edit
           'swiper
           'treemacs
-          'vterm
           'typescript-mode
+          'vterm
           'yaml-mode
           'yasnippet
+          ;; 'company-tabnine
           ))
   (straight-use-package wenpin-package))
 
