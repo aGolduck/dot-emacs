130053898c842c0901e24137bb5a87f01890ef9d
wenpin <wpchou@outlook.com>
2020-06-22 12:00:45 +0800

    (add-hook 'org-roam-capture-after-find-file-hook #'winner-undo)

--- config.el
+++ config.el
@@ -537,6 +537,7 @@ unwanted space when exporting org-mode to html."
   (setq org-roam-directory "~/org/roam"
         org-roam-completion-system 'ivy)
   (global-set-key (kbd "M-SPC n n") #'org-roam-find-file)
+  (add-hook 'org-roam-capture-after-find-file-hook #'winner-undo)
   :config
   (define-key org-roam-mode-map (kbd "M-SPC n l") #'org-roam)
   (define-key org-roam-mode-map (kbd "M-SPC n h") #'org-roam-jump-to-index)
