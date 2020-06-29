92651fc85f6a19aa3cb1894d3b07a7c69bd1af16
wenpin <wpchou@outlook.com>
2020-06-29 20:06:42 +0800

    add keybindngs for winner-mode

--- keybindings.el
+++ keybindings.el
@@ -61,6 +61,8 @@ Position the cursor at it's beginning, according to the current mode."
 (global-set-key (kbd "M-SPC w d") 'delete-window)
 (global-set-key (kbd "M-SPC w o") 'other-window)
 (global-set-key (kbd "M-SPC w s") 'split-window-right)
+(global-set-key (kbd "M-SPC w r") 'winner-redo)
+(global-set-key (kbd "M-SPC w u") 'winner-undo)
 (global-set-key (kbd "M-SPC w v") 'split-window-below)
 (global-set-key (kbd "M-SPC z H") #'hs-hide-all)
 (global-set-key (kbd "M-SPC z S") #'hs-show-all)
