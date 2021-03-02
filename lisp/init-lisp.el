f45ecb08b86cfa0f38cb8e195e96335129a0225c
wenpin <wpchou@outlook.com>
2021-03-02 11:37:44 +0800

    tweak lispy-mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -5,10 +5,13 @@
   :init
   (add-hook 'emacs-lisp-mode-hook #'lispy-mode)
   :config
+  (diminish 'lispy-mode)
   (define-key lispy-mode-map (kbd "[") nil)
   (define-key lispy-mode-map (kbd "]") nil)
   (define-key lispy-mode-map (kbd "M-a") #'lispy-backward)
   (define-key lispy-mode-map (kbd "M-e") #'lispy-forward)
-  (define-key lispy-mode-map (kbd "i") nil))
+  (define-key lispy-mode-map (kbd "i") nil)
+  (define-key lispy-mode-map (kbd "M-i") nil)
+  (define-key lispy-mode-map (kbd "M-o") nil))
 
 (provide 'init-lisp)
