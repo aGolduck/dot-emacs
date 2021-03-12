e0b58322e7e6f00f8cf6c25b540c0254b6475297
wenpin <wpchou@outlook.com>
2021-03-12 22:29:41 +0800

    tweak straight

--- lisp/w-straight.el
+++ lisp/w-straight.el
@@ -1,21 +1,33 @@
 ;;; bootstrap straight.el  -*- lexical-binding: t; -*-
 
+;; constants
 (defconst w/STRAIGHT (locate-user-emacs-file "straight"))
 (defconst w/STRAIGHT-REPOS (locate-user-emacs-file "straight/repos"))
 (defconst w/STRAIGHT-VERSIONS (locate-user-emacs-file "straight/versions"))
-(unless (file-exists-p w/STRAIGHT) (mkdir w/STRAIGHT))
-(unless (file-exists-p w/STRAIGHT-REPOS) (mkdir w/STRAIGHT-REPOS))
-(unless (file-exists-p w/STRAIGHT-VERSIONS) (mkdir w/STRAIGHT-VERSIONS))
 (setq straight-base-dir (expand-file-name (concat "straight-" emacs-version "-" (replace-regexp-in-string "/" "-" (symbol-name system-type))) w/STRAIGHT))
 (defconst w/STRAIGHT-SELF-DIR (expand-file-name "straight" straight-base-dir))
 (defconst w/STRAIGHT-REPOS-DIR (expand-file-name "repos" w/STRAIGHT-SELF-DIR))
 (defconst w/STRAIGHT-VERSIONS-DIR (expand-file-name "versions" w/STRAIGHT-SELF-DIR))
+
+;; directories
+(unless (file-exists-p w/STRAIGHT) (mkdir w/STRAIGHT))
+(unless (file-exists-p w/STRAIGHT-REPOS) (mkdir w/STRAIGHT-REPOS))
+(unless (file-exists-p w/STRAIGHT-VERSIONS) (mkdir w/STRAIGHT-VERSIONS))
 (unless (file-exists-p straight-base-dir) (mkdir straight-base-dir))
 (unless (file-exists-p w/STRAIGHT-SELF-DIR) (mkdir w/STRAIGHT-SELF-DIR))
 (unless (file-exists-p w/STRAIGHT-REPOS-DIR)
   (shell-command (concat "ln -s ../../repos " w/STRAIGHT-REPOS-DIR)))
 (unless (file-exists-p w/STRAIGHT-VERSIONS-DIR)
   (shell-command (concat "ln -s ../../versions " w/STRAIGHT-VERSIONS-DIR)))
+
+;; straight options
+(setq straight-repository-branch "master"
+      ;; straight-check-for-modifications
+      straight-use-package-by-default nil
+      straight-default-vc 'git
+      straight-vc-git-default-clone-depth 1)
+
+;; bootstrap straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (locate-user-emacs-file "straight/repos/straight.el/bootstrap.el"))
@@ -28,7 +40,5 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-(setq straight-default-vc 'git
-      straight-vc-git-default-clone-depth 1)
 
 (provide 'w-straight)
