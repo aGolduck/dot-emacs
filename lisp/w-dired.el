41da8460c192cb989215dce69787932aa47cb873
wenpin <wpchou@outlook.com>
2021-03-30 11:59:26 +0800

    refactor w-dired as a configuration template

--- lisp/w-dired.el
+++ lisp/w-dired.el
@@ -1,38 +1,54 @@
 ;;;  -*- lexical-binding: t; -*-
+;;; set up a template for complex configuration modules
+
+;;; 1. load third-party packages
+(straight-use-package 'dired-rsync)
+(straight-use-package 'dired-quick-sort)
+(straight-use-package 'dotenv-mode)
+(straight-use-package 'direnv)
+(straight-use-package 'ranger)
+
+;;; 2. autoload
+(autoload 'dired-jump "dired-x")
+(autoload 'hydra-dired-quick-sort/body "dired-quick-sort")
+
+;;; 3. customize variables
 (setq dired-listing-switches "-Afhlv"
       dired-auto-revert-buffer t
       dired-dwim-target t
       dired-recursive-copies 'always
-      dired-recursive-deletes 'top)
-
-;; dired-x
-(setq dired-guess-shell-alist-user '(("\\.doc\\'" "libreoffice")
+      dired-recursive-deletes 'top
+      dired-guess-shell-alist-user '(("\\.doc\\'" "libreoffice")
                                      ("\\.docx\\'" "libreoffice")
                                      ("\\.ppt\\'" "libreoffice")
                                      ("\\.pptx\\'" "libreoffice")
                                      ("\\.xls\\'" "libreoffice")
-                                     ("\\.xlsx\\'" "libreoffice")))
-(autoload 'dired-jump "dired-x")
-(global-set-key (kbd "M-SPC ^") #'dired-jump)
+                                     ("\\.xlsx\\'" "libreoffice"))
+      image-dired-dir (w/locate-emacs-var-file "image-dired"))
 
-(straight-use-package 'dired-rsync)
-(straight-use-package 'dired-quick-sort)
+;;; 4. lazy setup
 (with-eval-after-load 'dired
+  ;; 4.1 require packages
+  (require 'dired-x)
+  ;; 4.2 local mode map hook
+  (add-hook 'dired-mode-hook #'dired-quick-sort)
+  ;; 4.3 lazy set up
   ;; do not open extra dired buffer
   (put 'dired-find-alternate-file 'disabled nil)
+  ;; 4.4 local mode map keybindings set up
   (define-key dired-mode-map (kbd "RET") #'dired-find-alternate-file)
   (define-key dired-mode-map
     (kbd "^") (lambda () (interactive) (find-alternate-file "..")))
+  (define-key dired-mode-map (kbd "s") #'hydra-dired-quick-sort/body))
 
-  (require 'dired-x)
+;;; 5. global hooks
+(when (executable-find "direnv")
+  (add-hook 'after-init-hook #'direnv-mode))
 
-  ;; image-dired
-  (setq image-dired-dir (w/locate-emacs-var-file "image-dired"))
+;;; 6. global key bindings
+(global-set-key (kbd "M-SPC ^") #'dired-jump)
 
-  (autoload 'hydra-dired-quick-sort/body "dired-quick-sort")
-  (define-key dired-mode-map (kbd "s") #'hydra-dired-quick-sort/body)
-  (add-hook 'dired-mode-hook #'dired-quick-sort))
-
+;;; 7. extra third-party set up
 ;;; ranger
 (setq ranger-map-style 'emacs)
 (setq ranger-key (kbd "M-R"))
@@ -45,10 +61,5 @@
   (define-key ranger-emacs-mode-map (kbd "C-n") #'ranger-next-file)
   (define-key ranger-emacs-mode-map (kbd "C-p") #'ranger-prev-file))
 
-(straight-use-package 'dotenv-mode)
-(straight-use-package 'direnv)
-(when (executable-find "direnv")
-  (add-hook 'after-init-hook #'direnv-mode))
-
-
+;;; 8. provide feature
 (provide 'w-dired)
