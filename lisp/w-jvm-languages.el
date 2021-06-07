890fc9b0553ef97981439dd63f7e7c8e19df7a7d
wenpin <wpchou@outlook.com>
2021-06-07 10:24:15 +0800

    tweak clojure and scala

--- lisp/w-jvm-languages.el
+++ lisp/w-jvm-languages.el
@@ -6,9 +6,12 @@
 
 ;;; clojure
 (add-hook 'clojure-mode-hook #'electric-pair-local-mode)
+(add-hook 'clojure-mode-hook #'lispy-mode)
+(add-hook 'clojure-mode-hook #'company-mode)
 
 ;;; scala
 (add-to-list 'auto-mode-alist '("\\.sc\\'" . scala-mode))
 (add-hook 'scala-mode-hook #'electric-pair-local-mode)
+(add-hook 'clojure-mode-hook #'company-mode)
 
 (provide 'w-jvm-languages)
