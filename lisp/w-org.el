50e6585056e0eb4f1dd2a6d2e1598acc0b477c4d
wenpin <wpchou@outlook.com>
2021-06-07 10:20:34 +0800

    set org efforts

--- lisp/w-org.el
+++ lisp/w-org.el
@@ -145,6 +145,8 @@
 (with-eval-after-load 'org-agenda
   (define-key org-agenda-keymap (kbd "R") #'org-agenda-refile))
 
+(setq org-global-properties '(("Effort_ALL" . "0 0:05 0:10 0:20 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00")))
+
 ;;; org capture
 (setq org-capture-templates
       '(
