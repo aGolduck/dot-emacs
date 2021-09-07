a74dd33e7213f336436c2fdbfddd5aa88516472d
wenpin <wpchou@outlook.com>
2021-09-07 23:23:37 +0800

    bind undo-fu undo and redo

--- lisp/w-edit.el
+++ lisp/w-edit.el
@@ -11,6 +11,8 @@
 (setq expand-region-contract-fast-key "V")
 (global-set-key (kbd "M-SPC v") #'er/expand-region)
 
+(global-set-key (kbd "C-z") #'undo-fu-only-undo)
+(global-set-key (kbd "C-S-z") #'undo-fu-only-redo)
 
 (add-hook 'hexl-mode-hook #'view-mode)
 
