9724cae473cdfa13e02d9e83f30cc8b00a1e3122
wenpin <wpchou@outlook.com>
2022-10-09 06:49:18 +0800

    try lsp-bridge

--- init.el
+++ init.el
@@ -23,6 +23,24 @@
 (require 'w-minimal)
 (require 'w-core)
 (require 'w-to-be-core)
+
+(straight-use-package 'posframe)
+(straight-use-package 'markdown-mode)
+(straight-use-package 'yasnippet)
+(straight-use-package '(lsp-bridge :host github :repo "manateelazycat/lsp-bridge" :files ("*")))
+;; (yas-global-mode)
+;; (setq lsp-bridge-enable-log t
+;;       lsp-bridge-enable-debug t)
+;; org-babel js/shell 等不支持，原因不明
+(setq lsp-bridge-org-babel-lang-list '("css" "python"))
+(with-eval-after-load 'lsp-bridge
+  (define-key lsp-bridge-mode-map (kbd "M-.") #'lsp-bridge-find-def)
+  (define-key lsp-bridge-mode-map (kbd "M-,") #'lsp-bridge-return-from-def))
+(add-hook 'lsp-bridge-mode-hook #'yas-minor-mode)
+;; (add-hook 'emacs-lisp-mode-hook #'lsp-bridge-mode)
+(add-hook 'css-mode-hook #'lsp-bridge-mode)
+(add-hook 'js-mode-hook #'lsp-bridge-mode)
+(add-hook 'python-mode-hook #'lsp-bridge-mode)
 ;; (require 'w-full)
 
 ;;; local settings
