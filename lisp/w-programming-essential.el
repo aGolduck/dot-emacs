d4697cdccdb538c82772cb47c6a78dd974563154
wenpin <wpchou@outlook.com>
2023-01-10 22:19:36 +0800

    set up clojure cider lispy

--- lisp/w-programming-essential.el
+++ lisp/w-programming-essential.el
@@ -21,8 +21,15 @@
 (setq typescript-indent-leven 2)
 (straight-use-package 'go-mode)
 (straight-use-package 'scala-mode)
+
+;;; clojure
 (straight-use-package 'clojure-mode)
 (straight-use-package 'cider)
+;; lispy-clojure 依赖，https://github.com/abo-abo/lispy/blob/42a12aeee6e472c99c49c070070d8be6eb2c3c38/lispy-clojure.clj#L25
+;; 配置于 cider-jack-in-dependencies 而不是 lispy-cider-jack-in-dependencies 使得手动 cider-jack-in 后 lispy-eval 也能起作用
+;; (setq lispy-cider-jack-in-dependencies '(("org.clojure/tools.namespace" "1.3.0")))
+(setq cider-jack-in-dependencies '(("org.clojure/tools.namespace" "1.3.0")))
+
 (with-eval-after-load 'cider
   ;; odd is that cider depends on org-src-mode
   (require 'org-src))
