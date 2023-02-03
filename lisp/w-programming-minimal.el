408f3a76b6fabf3c4efc391c4b1af88500804249
wenpin <wpchou@outlook.com>
2023-02-03 15:30:26 +0800

    auto-mode-alist

--- lisp/w-programming-minimal.el
+++ lisp/w-programming-minimal.el
@@ -14,6 +14,18 @@
 (define-auto-insert "\.java" "with-Logger.java")
 (add-hook 'after-init-hook #'auto-insert-mode)
 
+
+;;; auto-mode-alist
+(add-to-list 'auto-mode-alist '("\\.cnf\\'" . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.gmk\\'" . makefile-mode))
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+
+;;; java
+;; java 文件经常巨长，打开行号方便定位
+(add-hook 'java-mode-hook #'display-line-numbers-mode)
+(add-hook 'java-mode-hook #'yas-minor-mode)
+
 ;;; js
 (setq js-indent-level 2)
 
@@ -21,9 +33,5 @@
 (setq nxml-child-indent 4
       nxml-attribute-indent 4
       lsp-xml-jar-file (expand-file-name (locate-user-emacs-file "resources/org.eclipse.lemminx-uber.jar")))
-
-;;; java
-(add-hook 'java-mode-hook #'display-line-numbers-mode)
-(add-hook 'java-mode-hook #'yas-minor-mode)
 
 (provide 'w-programming-minimal)
