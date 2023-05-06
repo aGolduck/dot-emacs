2de2d398a5c73230f3edb8d29b626070a88d2714
wenpin <wpchou@outlook.com>
2023-05-06 18:01:16 +0800

    优化 auto-insert-templates/with-logger.ts

--- auto-insert-templates/with-logger.ts
+++ auto-insert-templates/with-logger.ts
@@ -3,7 +3,7 @@ import { parse } from "https://deno.land/std/flags/mod.ts";
 const flags = parse(Deno.args, {
   string: ["std-log-level"]
 })
-const STD_LOG_LEVEL = flags["std-log-level"] || "INFO"
+const STD_LOG_LEVEL = flags["std-log-level"] as "DEBUG" | "INFO" | "ERROR" | "NOTSET" | "WARNING" | "CRITICAL" || "INFO"
 await log.setup({
   handlers: {
     console: new log.handlers.ConsoleHandler(STD_LOG_LEVEL),
