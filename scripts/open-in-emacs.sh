3ce6e30325cc6e323634524bb433f96efa873e5e
wenpin <wpchou@outlook.com>
2023-07-31 11:20:55 +0800

    open-in-emacs.sh extract variable evar_in_emacs

--- scripts/open-in-emacs.sh
+++ scripts/open-in-emacs.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
+## 可以用于 Intillij IDEA external tools 调用
+## 参数: "$FilePath$" $LineNumber$ $ColumnNumber$
+## 双引号处理文件名中间有空格情况
+
 file=$1
 line=$2
 col=$3
 
-# echo "(progn
-#        (find-file \"$file\")
-#        (when (not (string= \"\" \"$line\"))
-#          (goto-char (point-min))
-#          (forward-line (- $line 1))
-#          (forward-char (- $col 1))
-#        )
-#        (select-frame-set-input-focus (selected-frame))
-#        (auto-revert-mode t))"
-
-emacsclient -n -e "(progn
+eval_in_emacs="(progn
        (find-file \"$file\")
        (when (not (string= \"\" \"$line\"))
          (goto-char (point-min))
@@ -23,3 +17,7 @@ emacsclient -n -e "(progn
        )
        (select-frame-set-input-focus (selected-frame))
        (auto-revert-mode t))"
+
+echo ${eval_in_emacs}
+
+emacsclient -n -e "${eval_in_emacs}"
