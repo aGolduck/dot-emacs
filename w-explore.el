c8982893400b798f6500cabe0ed9997f5cc26b6b
wenpin <wpchou@outlook.com>
2023-08-30 16:35:33 +0800

    comment or uncomment console logging

--- w-explore.el
+++ w-explore.el
@@ -8,4 +8,15 @@
   "Write a greeting"
   "Type name of user: "
   "hello, " str "!")
+
+(defun w/uncomment-console-logging ()
+  (interactive)
+  (beginning-of-buffer)
+  (replace-string "// console(" "// console"))
+
+(defun w/comment-out-console-logging ()
+    (interactive)
+  (beginning-of-buffer)
+  (replace-string "console(" "// console("))
+
 (provide 'w-explore)
