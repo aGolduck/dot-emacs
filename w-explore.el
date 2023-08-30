1043816a4bd435e3d417b063a11568258d70bfcb
wenpin <wpchou@outlook.com>
2023-08-30 16:34:52 +0800

    define-skeleton gpt-template

--- w-explore.el
+++ w-explore.el
@@ -1,1 +1,11 @@
+(define-skeleton gpt-template
+  "docstring: chatgpt template"
+  "prompt"
+  "you are chatgpt"
+  )
+
+(define-skeleton hello-world-skeleton
+  "Write a greeting"
+  "Type name of user: "
+  "hello, " str "!")
 (provide 'w-explore)
