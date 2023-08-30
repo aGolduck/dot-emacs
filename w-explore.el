5521d1a8acb3501d65261b60e09e152e2dc579ff
wenpin <wpchou@outlook.com>
2023-08-30 16:36:34 +0800

    treesitter related setting

--- w-explore.el
+++ w-explore.el
@@ -19,4 +19,19 @@
   (beginning-of-buffer)
   (replace-string "console(" "// console("))
 
+
+(when (>= emacs-major-version 29)
+  (straight-use-package 'clojure-ts-mode)
+  (add-hook 'clojure-ts-mode #'lispy-mode)
+
+  (add-to-list 'auto-mode-alist '("\\.sy\\'" . json-ts-mode))
+  (straight-use-package 'treesit-auto)
+  (require 'treesit-auto)
+  (global-treesit-auto-mode)
+
+  (straight-use-package 'combobulate)
+  (autoload 'combobulate-mode "combobulate" nil t nil)
+  (add-hook 'typescript-ts-mode-hook #'combobulate-mode)
+  (add-hook 'json-ts-mode-hook #'combobulate-mode))
+
 (provide 'w-explore)
