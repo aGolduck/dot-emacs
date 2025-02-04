7f287dc3caa3e58d808d3ef4a4e06a37d36dc651
wpchou <wpchou@outlook.com>
2025-02-04 16:46:37 +0800

    update with-logger.ts

--- auto-insert-templates/with-logger.ts
+++ auto-insert-templates/with-logger.ts
@@ -1,12 +1,12 @@
-import * as log from "https://deno.land/std/log/mod.ts";
-import { parse } from "https://deno.land/std/flags/mod.ts";
+import * as log from "jsr:@std/log";
+import { parse } from "jsr:@std/flags";
 const flags = parse(Deno.args, {
   string: ["std-log-level"]
 })
 const STD_LOG_LEVEL = flags["std-log-level"] as "DEBUG" | "INFO" | "ERROR" | "NOTSET" | "WARNING" | "CRITICAL" || "INFO"
 await log.setup({
   handlers: {
-    console: new log.handlers.ConsoleHandler(STD_LOG_LEVEL),
+    console: new log.ConsoleHandler(STD_LOG_LEVEL),
   },
   loggers: {
     default: {
