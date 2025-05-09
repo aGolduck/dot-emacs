8680a5471bab091f63de16d232361711008f0637
wpchou <wpchou@outlook.com>
2025-05-09 15:18:14 +0800

    调整 deepseek 配置

--- w-explore.el
+++ w-explore.el
@@ -1,10 +1,28 @@
+(defalias 'project-root-dired 'project-dired)
+
 (require 'auth-source)
 (setenv "DEEPSEEK_API_KEY" (auth-source-pick-first-password :host "deepseek.com"))
+(setenv "AIDER_COMMIT_PROMPT" "You are an expert software engineer that generates concise, \
+one-line Git commit messages based on the provided diffs.
+Review the provided context and diffs which are about to be committed to a git repo.
+Review the diffs carefully.
+Generate a one-line commit message for those changes.
+The commit message should be structured as follows: [module] <type>: <description>
+Use these for <type>: fix, feat, build, chore, ci, docs, style, refactor, perf, test
+
+Ensure the commit message:
+- Starts with the appropriate prefix.
+- 使用中文写 commit 消息
+- Is in the imperative mood (e.g., \"Add feature\" not \"Added feature\" or \"Adding feature\").
+- Does not exceed 72 characters.
+- commit 有详情列表，且不要超过三条
+")
 
 (straight-use-package '(aidermacs :host github :repo "MatthewZMD/aidermacs"))
 ;; (setq aidermacs-backend 'vterm)
 (setq aidermacs-default-model "deepseek")
-(setq aidermacs-use-architect-mode t)
+;; deepseek-reasoner 和 deepseek-chat 的组合作用不大
+;; (setq aidermacs-use-architect-mode t)
 (setq aidermacs-architect-model "deepseek/deepseek-reasoner")
 (setq aidermacs-editor-model "deepseek/deepseek-chat")
 (setq aidermacs-show-diff-after-change nil)
